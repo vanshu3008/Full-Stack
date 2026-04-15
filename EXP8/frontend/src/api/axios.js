29daeb08de6b85f607f110395ea8c46ae687afdf
vanshu3008 <vvpandey09@gmail.com>
2026-04-15 17:27:43 +0530

    Update axios.js

--- EXP8/frontend/src/api/axios.js
+++ EXP8/frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000"
+  baseURL: "https://student-backend-6trl.onrender.com"
 });
 
 instance.interceptors.request.use((config) => {
